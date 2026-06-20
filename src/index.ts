079a28613aa17df5c9aff430a8d916865dabcbd0
Adroaldo <hygino@msn.com>
2026-06-20 14:50:43 -0300

    Express Body-Parser

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,22 @@
-import express from 'express';
 import type { Request, Response } from 'express';
+import express from 'express';
 
 const app = express();
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
 const port = 3000;
 
 app.get('/hello', (req: Request, res: Response) => {
     res.send('Hello, World!');
+});
+
+
+app.post('/echo', (req: Request, res: Response) => {
+    res.send({
+        data: req.body,
+        message: 'Data received successfully'
+    });
 });
 
 app.listen(port, () => {
